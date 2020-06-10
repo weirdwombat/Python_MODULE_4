d797e28a4245324f0801c4d072f971f9854bb48a
weirdwombat <omkennedy@dmacc.edu>
2020-06-10 18:02:51 -0500

    Final Program

--- input_validation/validation_with_try.py
+++ input_validation/validation_with_try.py
@@ -1,16 +1,22 @@
+"""
+Program : validation_with_try.py
+Author : Olivia Kennedy
+Date Last Modified : 06/10/2020
+This program will accept the input of 3 positive scores and provide an output of the scores.
+Bad input (negative numbers) will cause ValueError(s), as well as prompt the user to try again.
+"""
 
+#defines average function and creates an exception for bad input
 def average(score1, score2, score3):
     try:
         if score1 < 0:
             raise ValueError
-    except ValueError:
-        print("Please try again and enter a positive number for score 1 to get your average.")
-        raise ValueError
-    try:
         if score2 < 0:
             raise ValueError
+        if score3 < 0:
+            raise ValueError
     except ValueError:
-        print("Please try again and enter a positive number for score 2 to get your average.")
+        print("Please try again and make sure to enter positive numbers for all of the scores to get your average.")
         raise ValueError
     else:
         return float((score1 + score2 + score3)/NUMBER_TESTS)
