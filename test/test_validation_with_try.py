63fcda955f6bc96d41f56996c9a3679ef6b41a73
weirdwombat <omkennedy@dmacc.edu>
2020-06-10 17:47:00 -0500

    Got 2nd test to fail

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -2,9 +2,14 @@ import unittest
 from input_validation import validation_with_try
 
 class MyTestCase(unittest.TestCase):
+
     def test_average_exception(self):
         with self.assertRaises(ValueError):
             validation_with_try.average(-90, 89, 78)
+
+    def test_average_exception_2(self):
+        with self.assertRaises(ValueError):
+            validation_with_try.average(90,- 89, 78)
 
 
 if __name__ == '__main__':
