0a71897a34383fca2c0de2b15b35535d27b366e5
weirdwombat <omkennedy@dmacc.edu>
2020-06-10 17:24:06 -0500

    Got First Test to Pass

---
+++ input_validation/validation_with_try.py
@@ -0,0 +1,20 @@
+
+def average(score1, score2, score3):
+    try:
+        if score1 < 0:
+            raise ValueError
+    except ValueError:
+        print("Please try again and enter a positive number for the score to get your average.")
+        raise ValueError
+    else:
+        return float((score1 + score2 + score3)/NUMBER_TESTS)
+
+
+if __name__ == '__main__':
+    NUMBER_TESTS = 3
+    score1 = float(input("Enter the first score:"))
+    score2 = float(input("Enter the second score:"))
+    score3 = float(input("Enter the third score:"))
+    print("Average Grade:",average(score1, score2, score3), "out of 100")
+
+
