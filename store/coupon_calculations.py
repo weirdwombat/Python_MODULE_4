124e5233d2aee51b6fbb25fe20d47a76543438ea
weirdwombat <omkennedy@dmacc.edu>
2020-06-10 19:23:48 -0500

    Got first round of tests to pass

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,12 +1,14 @@
 def calculate_price(price, cash_coupon, percent_coupon):
-    pass
-
-def shipping():
-    pass
-
-def tax():
-    pass
-
+    TAX = 1.06
+    subtotal = (price - cash_coupon) * (1 - percent_coupon)
+    if subtotal < 10:
+        price_with_shipping = subtotal + 5.95
+    final_price = round((price_with_shipping * TAX), 2)
+    return final_price
 
 if __name__ == '__main__':
-    pass
+    price = float(input("How much is this purchase:"))
+    cash_coupon = float(input("Do you have a cash off coupon, if so is it worth 5 or 10 dollars"))
+    percent_coupon = float(input("Enter the amount of your percent off discount as its decimal. (If you have 20% off you would answer '.20' "))
+    print("Your order costs", calculate_price, "dollars")
+
